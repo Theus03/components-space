cca683e933f6ad309723561e572cb43ca1014ad0
Matheus Leite <matheusssleite@outlook.com>
2025-03-10 08:54:02 -0300

    Structure in page section buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { CardComponent } from "./components/card/card.component";
 import { PrimeNG } from 'primeng/config';
+import { ButtonsComponent } from "./components/buttons/buttons.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, CardComponent],
+  imports: [HeaderComponent, CardComponent, ButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
