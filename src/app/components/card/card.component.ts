9694ce655607fed48452d6cb128eef06fbc279dd
Matheus Leite <matheusssleite@outlook.com>
2025-03-10 08:48:09 -0300

    Creating group of cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SmallComponent } from "./small/small.component";
 import { MediumComponent } from "./medium/medium.component";
+import { LargeComponent } from "./large/large.component";
 
 @Component({
   selector: 'app-card',
-  imports: [SmallComponent, MediumComponent],
+  imports: [SmallComponent, MediumComponent, LargeComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
