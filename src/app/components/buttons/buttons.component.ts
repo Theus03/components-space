ca2b743a2a99369291027e2d08b6d83030a5d1d7
Matheus Leite <matheusssleite@outlook.com>
2025-03-11 08:57:33 -0300

    Finish area buttons

--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { SmallComponent } from './small/small.component';
+import { MediumComponent } from './medium/medium.component';
+import { LargeComponent } from "./large/large.component";
 
 @Component({
   selector: 'app-buttons',
-  imports: [],
+  imports: [SmallComponent, MediumComponent, LargeComponent],
   templateUrl: './buttons.component.html',
   styleUrl: './buttons.component.css'
 })
---
+++ src/app/components/buttons/medium/medium.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medium',
+  imports: [],
+  templateUrl: './medium.component.html',
+  styleUrl: './medium.component.css'
+})
+export class MediumComponent {
+
+}
