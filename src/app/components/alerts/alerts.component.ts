1f412e74663cf858040712a5d1f23e105a83c97b
Matheus Leite <matheusssleite@outlook.com>
2025-03-12 07:53:36 -0300

    Finish area alerts in page

--- src/app/components/alerts/alerts.component.ts
+++ src/app/components/alerts/alerts.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SmallComponent } from './small/small.component';
+import { LargeComponent } from "./large/large.component";
 
 @Component({
   selector: 'app-alerts',
-  imports: [],
+  imports: [SmallComponent, LargeComponent],
   templateUrl: './alerts.component.html',
   styleUrl: './alerts.component.css'
 })
---
+++ src/app/components/alerts/small/small.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small',
+  imports: [],
+  templateUrl: './small.component.html',
+  styleUrl: './small.component.css'
+})
+export class SmallComponent {
+
+}
