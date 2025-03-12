04c81b7f6e910185e8a5b5791624aea030970966
Matheus Leite <matheusssleite@outlook.com>
2025-03-12 07:37:48 -0300

    Init section alerts in page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from "./components/header/header.component";
 import { CardComponent } from "./components/card/card.component";
 import { PrimeNG } from 'primeng/config';
 import { ButtonsComponent } from "./components/buttons/buttons.component";
+import { AlertsComponent } from "./components/alerts/alerts.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, CardComponent, ButtonsComponent],
+  imports: [HeaderComponent, CardComponent, ButtonsComponent, AlertsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
