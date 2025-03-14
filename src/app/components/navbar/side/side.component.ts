ac56710f0502d9e46a6050e9e9637a86ef321c87
Matheus Leite <matheusssleite@outlook.com>
2025-03-14 16:18:19 -0300

    Implementing copyCode in navbar

--- src/app/components/navbar/side/side.component.ts
+++ src/app/components/navbar/side/side.component.ts
@@ -7,5 +7,65 @@ import { Component } from '@angular/core';
   styleUrl: './side.component.css'
 })
 export class SideComponent {
+  sideComponent = {
+    template: `
+      <style>
+      .navbar {
+        background-color: #F9F9F9;
+        height: 450px;
+        width: 100px;
+        border-radius: 12px;
+    }
+    
+    nav {
+        height: 450px;
+    }
+    
+    ul {
+        height: 450px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 4rem;
+    }
+    
+    li {
+        transition: 0.4s ease-in-out;
+        cursor: pointer;
+    }
+    
+    li.active {
+        background-color: black;
+        padding: 0.6rem;
+        border-radius: 8px;
+    }
+    
+    li:not(.active):hover {
+        background-color: rgb(226, 226, 226);
+        padding: 1rem;
+        border-radius: 8px;
+    }
+    
+    
+    img {
+        width: 2rem;
+    }
+      </style>
+      <div class="navbar">
+        <nav>
+            <ul>
+                <li class="active"><img src="assets/images/home-2-line.png" alt="Home"></li>
+                <li><img src="assets/images/archive-stack-line.png" alt="Stack"></li>
+                <li><img src="assets/images/folder-info-line.png" alt="Folder"></li>
+            </ul>
+        </nav>
+    </div>
+    `
+  }
 
+  copySide = () => {
+    navigator.clipboard.writeText(this.sideComponent.template);
+    alert("✅ Código copiado com sucesso!");
+  }
 }
--- src/app/components/navbar/top/top.component.ts
+++ src/app/components/navbar/top/top.component.ts
@@ -7,5 +7,72 @@ import { Component } from '@angular/core';
   styleUrl: './top.component.css'
 })
 export class TopComponent {
+  topComponent = { 
+    template: `
+      <style>
+        .navbar {
+          background-color: #F9F9F9;
+          width: 800px;
+          height: 100px;
+          border-radius: 12px;
+        }
 
+        nav {
+          height: 100px;
+        }
+
+        ul {
+          height: 100px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 4rem;
+        }
+
+        li {
+          display: flex;
+          align-items: center;
+          gap: 1rem;
+          justify-content: center;
+          transition: 0.4s ease-in-out;
+          cursor: pointer;
+        }
+
+        li.active {
+          background-color: black;
+          padding: 1rem;
+          border-radius: 8px;
+        }
+
+        li.active > span {
+          color: white;
+        }
+
+        li:not(.active):hover {
+          background-color: rgb(226, 226, 226);
+          padding: 1rem;
+          border-radius: 8px;
+        }
+
+        img {
+          width: 1.6rem;
+        }
+      </style>
+      <div class="navbar">
+    <nav>
+        <ul>
+            <li class="active"><img src="assets/images/home-2-line.png" alt="Home"><span>Home</span></li>
+            <li><img src="assets/images/archive-stack-line.png" alt="Stack"><span>Components</span></li>
+            <li><img src="assets/images/folder-info-line.png" alt="Folder"><span>Sobre</span></li>
+        </ul>
+    </nav>
+</div>
+    `
+
+  }
+
+  copyTop = () => {
+    navigator.clipboard.writeText(this.topComponent.template);
+    alert("✅ Código copiado com sucesso!");
+  }
 }
