fa163588ffbbf7a44325b1272b8753c449b967bb
Matheus Leite <matheusssleite@outlook.com>
2025-03-14 11:46:10 -0300

    Colocando lógica de cópia do botão

--- src/app/components/buttons/small/small.component.ts
+++ src/app/components/buttons/small/small.component.ts
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './small.component.css'
 })
 export class SmallComponent {
+  smallComponent = {
+    template: `
+      <style>
 
+button {
+  width: 100%;
+  background-color: black;
+  border: none;
+  border-radius: 8px;
+  padding: 0.8rem;
+  color: white;
+  font-family: "Inter";
+  font-size: 1.4rem;
+  font-weight: 400;
+  text-align: center;    
+  cursor: pointer;
+  transition: 0.4s ease-in-out;
+}
+
+button:hover {
+  transform: translateX(5%);
+}
+      </style>
+      <button type="button">Small</button>
+    `
+  }
+
+  copySmall = () => {
+    navigator.clipboard.writeText(this.smallComponent.template);
+    alert("✅ Código copiado com sucesso!");
+  }
 }
